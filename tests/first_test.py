03406c939a63411cbce22851a3bff1fbefd82ae4
VladimirKozlov466 <86114164+VladimirKozlov466@users.noreply.github.com>
2021-10-01 13:25:24 +0500

    my own update

--- tests/first_test.py
+++ tests/first_test.py
@@ -1,19 +1,24 @@
 import pytest
 from app.calcilator import Calculator
 
+
 class TestCalc:
     def setup(self):
         self.calc = Calculator
+
     def test_multiply_calculate_correctly(self):
-        assert self.calc.multiply(self, 2, 2) == 4
+        """Проверка операции умножения первую переменную умножает на вторую"""
+        assert self.calc.multiply(self, 2, 5) == 10
 
     def test_division_calculate_correctly(self):
+        """Проверка операции деления первую переменную делит на вторую"""
         assert self.calc.division(self, 6, 3) == 2
+        # Проверка деления на "ноль" в калькуляторе отсутствует
 
     def test_subtraction_calculate_correctly(self):
-        assert self.calc.subtraction(self, 8, 4) == 4
+        """Проверка операции вычитания из первой переменной вычитается вторая"""
+        assert self.calc.subtraction(self, 8, 12) == -4
 
     def test_adding_calculate_correctly(self):
+        """Проверка операции сложения первая складывается со второй"""
         assert self.calc.adding(self, 4, 2) == 6
-
-
